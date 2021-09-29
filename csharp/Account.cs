6204f492bc070988b9cd11aabad932e7191f5799
Armin Moshayedi <moshayedi@gmail.com>
2021-09-28 20:31:25 -0400

    Account class added

---
+++ csharp/Account.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp
+{
+    public class AccountBuilder
+    {
+        public class Account
+        {
+            public int Balance { get; set; }
+            public string Name { get; set; }
+            public string Type { get; set; }
+
+
+
+        }
+
+        Account _account;
+        AccountBuilder()
+        {
+            _account = new Account();
+        }
+        public static AccountBuilder DefaultAccount(string Name, int balance)
+        {
+            var accountBuilder = new AccountBuilder();
+            accountBuilder._account.Balance = balance;
+            accountBuilder._account.Name = Name;
+            return accountBuilder;
+        }
+        public AccountBuilder AsVip()
+        {
+            _account.Type = "VIP";
+            return this;
+        }
+        public Account Build()
+        {
+            return _account;
+        }
+    }
+   
+    
+}
